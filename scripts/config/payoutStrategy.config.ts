dbf20da50da5d3f84a8246849a32e74d65e56e3d
Lisandro Corbalan <lmcorbalan@gmail.com>
2023-06-26 12:15:14 -0300

    Goerli deployment

--- scripts/config/payoutStrategy.config.ts
+++ scripts/config/payoutStrategy.config.ts
@@ -1,11 +1,11 @@
 // Update this file any time a new Payout Strategy contract has been added
-type MerklePayoutParams = {
+type PayoutParams = {
   factory: string;
   implementation: string;
   contract: string;
 };
 
-type DeployParams = Record<string, MerklePayoutParams>;
+type DeployParams = Record<string, PayoutParams>;
 
 export const MerklePayoutParams: DeployParams = {
   mainnet: {
@@ -16,11 +16,39 @@ export const MerklePayoutParams: DeployParams = {
   goerli: {
     factory: "0xE2Bf906f7d10F059cE65769F53fe50D8E0cC7cBe",
     implementation: "0xC808c9Ea4020E0F6Ec20715EEA0642fA6870B5Cc",
-    contract: "0x3172a6cCE26529e7DD2B533e7c3622a0b544f349",
+    contract: "0xCB64FFd025384E6353C1923Ce5Bda511229e2E92",
   },
   "optimism-mainnet": {
     factory: "0xB5365543cdDa2C795AD104F4cB784EF3DB1CD383",
     implementation: "0x0C1B55f485dF69cc10527995c48883438aD3fC4A",
+    contract: "",
+  },
+  "fantom-mainnet": {
+    factory: "",
+    implementation: "",
+    contract: "",
+  },
+  "fantom-testnet": {
+    factory: "",
+    implementation: "",
+    contract: "",
+  },
+};
+
+export const DirectPayoutParams: DeployParams = {
+  mainnet: {
+    factory: "",
+    implementation: "",
+    contract: "",
+  },
+  goerli: {
+    factory: "0x51cDB7de5476e3B85Ca76FEcD63862adc343aAe6",
+    implementation: "0xAEd1cE441FA6aD4F89d28026F3E7491394deEa5F",
+    contract: "0xe14c00F31fBF325FE35f461e6cb821286F6cF040",
+  },
+  "optimism-mainnet": {
+    factory: "",
+    implementation: "",
     contract: "",
   },
   "fantom-mainnet": {
